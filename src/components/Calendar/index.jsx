0edc80d2b11b07bc37852c9b58f34bf244634f8f
naastyaer <ershova120101@gmail.com>
2023-03-30 13:51:40 +0400

    правки для развертывания сайта

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
-
-
-
 const Calendar=({value, onChange}) => {
   return (
     <div>
       <p>Выберите дату</p>
-      <DatePicker selected={value} onChange={(date) => onChange(date)} className="text-black mb-2 w-full rounded-sm bg-slate-100 p-1" />
+      <DatePicker selected={value} onChange={(date) => onChange(date)} className="text-black mb-2 w-full rounded-sm bg-slate-100 p-1"/>
       {console.log(value)}
       
     </div>
